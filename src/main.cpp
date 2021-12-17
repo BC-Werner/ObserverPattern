600b0becf5e6bbbc38d8c6986ba25d0fc23c52b7
Brandon Werner <47579647+BC-Werner@users.noreply.github.com>
2021-12-16 21:32:09 -0500

    Main Boilerplate

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Brandon Werner
+ * 12/5/2021
+ *
+ * Observer Design Pattern Example
+ */
+
+#include <iostream>
+ //#include <vector>
+ //#include <string>
+ //#include <algorithm>
+#include "Observer.hpp"
+
+//using namespace std;
+
+int main()
+{
+
+
+
+    return 0;
+}
