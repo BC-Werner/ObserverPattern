4d5306e20a0b5e1e3932f6733df3df69b889fa04
Brandon Werner <47579647+BC-Werner@users.noreply.github.com>
2021-12-22 20:04:18 -0500

    Set up a basic subject

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,19 @@
  //#include <vector>
  //#include <string>
  //#include <algorithm>
-#include "Observer.hpp"
+#include "ConcreteSubject.hpp"
+#include "ConcreteObserver.hpp"
+#include "DataClass.hpp"
 
 //using namespace std;
 
 int main()
 {
 
+    ConcreteSubject subject;
+
+    subject.setData(123, 3.14, 'B');
+    subject.setData(DataClass{ 456, 5.34, 'A' });
 
 
     return 0;
